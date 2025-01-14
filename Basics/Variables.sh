25bb9f10b891a418cdb24a9151ec6c40efc927bf
SURIYA-R-K <suriyarkds@gmail.com>
2025-01-14 23:00:42 +0530

    Create Variables.sh

---
+++ Basics/Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Defining variables
+name="Your Name"
+#just giving some random age
+age=100
+
+# to print this we are using the echo command
+echo "My name is ${name} and my age is ${age} "
+
