319347d84c631a74adf0d0a03c345d71aa8f8be0
SURIYA-R-K <suriyarkds@gmail.com>
2025-01-14 22:56:26 +0530

    Create Comments.sh

---
+++ Basics/Comments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo " For single line comment we use the symbole # "
+# Example : This is a single line comment
+echo " For multi line comment is the following symbols << comment ..... your comment ..... comment"
+<<comment
+hello all this
+is 
+a multi line comment
+comment
